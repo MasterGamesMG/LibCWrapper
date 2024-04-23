582df832e34d70c509bc21c5265dc9fa3fdd0203
Master Games <90146578+MasterGamesMG@users.noreply.github.com>
2024-04-23 22:01:32 +0200

    Demo for x86 and x64
    Logs below with current functions implemented :
    
    LibCWrapper Loaded! => (00000078C46FFA24)
    
    my_safe_isalnum('A'): 1
    my_safe_isalnum('*'): 0
    my_safe_isalpha('a'): 1
    my_safe_isalpha('1'): 0
    my_safe_isblank(' '): 1
    my_safe_isblank('a'): 0
    my_safe_iscntrl('\n'): 1
    my_safe_iscntrl('A'): 0
    my_safe_isdigit('7'): 1
    my_safe_isdigit('A'): 0
    my_safe_isgraph('a'): 1
    my_safe_isgraph(' '): 0
    my_safe_islower('a'): 1
    my_safe_islower('A'): 0
    my_safe_isprint('a'): 1
    my_safe_isprint('\n'): 0
    my_safe_ispunct('.'): 1
    my_safe_ispunct('A'): 0
    my_safe_isspace(' '): 1
    my_safe_isspace('a'): 0
    my_safe_isupper('A'): 1
    my_safe_isupper('a'): 0
    my_safe_isxdigit('F'): 1
    my_safe_isxdigit('G'): 0
    my_safe_tolower('A'): 97
    my_safe_tolower('3'): 51
    my_safe_toupper('a'): 65
    my_safe_toupper('9'): 57
    my_safe_abs(-5): 5
    my_safe_abs(5): 5
    my_safe_atof("3.14"): 3.14
    my_safe_atof("hello"): 0
    my_safe_atoi("123"): 123
    my_safe_atoi("abc"): 0

---
+++ examples/x86_and_x64_project/Demo/main.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <thread>
+
+#include "../../../LibCWrapper/LibCWrapper.h"
+
+using namespace std;
+
+int main()
+{
+    auto wrapperThread = []()
+    {
+        LibCWrapper wrapper;
+        wrapper.logAvailableFunctions();
+    };
+
+    std::thread thread(wrapperThread);
+    thread.join();
+
+	return 0;
+}
